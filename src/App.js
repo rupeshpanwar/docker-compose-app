8b992a326e8f44137fcf8a6f3daea490297a118e
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-08-16 10:54:02 +0530

    deploy app

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello DevOps master!!!
+          Hello DevOps masterclass!!!
         </a>
       </header>
     </div>
