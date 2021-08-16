2642f8825619a6b442c0a7ccff410f43978a1bab
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-08-16 11:06:39 +0530

    switch to github

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello DevOps masterclass!!!
+          Hello DevOps master!!!
         </a>
       </header>
     </div>
